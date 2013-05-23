53988863530efa01d9f1e6759ed79b5e3e406100
mberg <markus.berg@hs-wismar.de>
2013-05-23 15:16:03 +0200

    added new constrcutor

--- src/net/mmberg/nadia/processor/dialogmodel/ActionResultMapping.java
+++ src/net/mmberg/nadia/processor/dialogmodel/ActionResultMapping.java
@@ -4,4 +4,12 @@ import net.mmberg.nadia.dialogmodel.definition.ActionResultMappingModel;
 
 public class ActionResultMapping extends ActionResultMappingModel {
 
+	public ActionResultMapping(){
+		super();
+	}
+	
+	public ActionResultMapping(String resultVarName, String resultValue, String message, String redirectToTask){
+		super(resultVarName,resultValue,message,redirectToTask);
+	}
+	
 }
